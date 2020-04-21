948455578da4dc8f9d9357e1498968f46af78a9e
Donal Maher <dm0860777682@gmail.com>
2020-04-21 15:34:23 +0100

    tiding the reso

---
+++ obsolete/Fishers_Iris_Analysis_summary_Test.py
@@ -0,0 +1,14 @@
+import analysis
+#Create historgram
+analysis.historgram("sepal_length")
+analysis.historgram("sepal_width")
+analysis.historgram("petal_length")
+analysis.historgram("petal_width")
+#Create scatterplot
+analysis.scatterplot("sepal_length","sepal_width")
+analysis.scatterplot("petal_length","petal_width")
+#Create summary.txt
+analysis.writeToAFile
+#analysis.std_plot
+
+
